0806c738adb201cedb3f1c28be77830a1c0383ca
Maximilien Vanstaevel <maximilien.vanstaevel@epitech.eu>
2023-09-19 12:07:47 +0200

    [+] add Level interface

---
+++ src/Level/Base/Level.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Entity.hpp"
+
+class Level
+{
+public:
+	Level() = default;
+	virtual ~Level() = default;
+
+	void LevelUpdate(const float dt)
+	{
+		for (auto& entity : _entities)
+		{
+			entity->EntityUpdate(dt);
+		}
+		update(dt);
+	}
+	void LevelDraw(sf::RenderTarget& target)
+	{
+		for (auto& entity : _entities)
+		{
+			entity->EntityDraw(target);
+		}
+		draw(target);
+	}
+
+	virtual void update(const float dt) = 0;
+	virtual void draw(sf::RenderTarget& target) = 0;
+
+	template <typename T>
+	void addEntity(void)
+	{
+		std::shared_ptr<T> entity = std::make_shared<T>();
+		_entities.push_back(entity);
+	}
+
+	template <typename T>
+	void removeEntity(void)
+	{
+		for (auto it = _entities.begin(); it != _entities.end(); ++it)
+		{
+			if (std::shared_ptr<T> ptr = std::dynamic_pointer_cast<T>(*it))
+			{
+				_entities.erase(it);
+				return;
+			}
+		}
+	}
+
+	template <typename T>
+	std::shared_ptr<T> getEntity()
+	{
+		for (auto& entity : _entities)
+		{
+			if (std::shared_ptr<T> ptr = std::dynamic_pointer_cast<T>(entity))
+			{
+				return ptr;
+			}
+		}
+		return nullptr;
+	}
+
+
+	// operator
+	bool operator==(const Level& other) const { return this == &other; }
+
+	protected:
+		std::vector<std::shared_ptr<Entity>> _entities;
+};
