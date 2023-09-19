ff9d21deab2167f5c83e73c955c184146a332e3f
Maximilien Vanstaevel <maximilien.vanstaevel@epitech.eu>
2023-09-19 12:10:16 +0200

    [+] add include all sfml in one hpp

---
+++ src/SFML.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** SFML
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
