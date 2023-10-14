74449223f3289c76ac85d898239269fe200d06ba
Alan Piedbois <alan@toboro.io>
2023-10-15 01:57:40 +0200

    [+] added drawableComp to factory

--- src/Component/Component.cpp
+++ src/Component/Component.cpp
@@ -19,16 +19,26 @@ void ECS::Component::FactoryAssignPlayerInputComponent(Entity* ent, std::string 
 	}
 }
 
+void ECS::Component::FactoryAssignDrawableComponent(Entity* ent, std::string type, std::string data) {
+	auto id = Utils::getRegisteredComponent(Utils::getTypeId<SpriteComponent>());
+	if (type == id) {
+		auto sprite = ent->assign<SpriteComponent>();
+		sprite->fromString(data);
+	}
+}
+
 void ECS::Component::FactoryAssignComponent(Entity* ent, std::string type, std::string str) {
 	// remove useless space in type
 	type.erase(std::remove_if(type.begin(), type.end(), isspace), type.end());
 	std::vector<std::function<void(Entity*, std::string, std::string)>> funcs = {
 		FactoryAssignPvComponent,
-		FactoryAssignPlayerInputComponent
+		FactoryAssignPlayerInputComponent,
+		FactoryAssignDrawableComponent
 	};
 	std::vector<std::string> id = {
 		Utils::getRegisteredComponent(Utils::getTypeId<PvComponent>()),
-		Utils::getRegisteredComponent(Utils::getTypeId<PlayerInputComponent>())
+		Utils::getRegisteredComponent(Utils::getTypeId<PlayerInputComponent>()),
+		Utils::getRegisteredComponent(Utils::getTypeId<SpriteComponent>())
 	};
 	// pour une raison inconnue (probablement le combo unordered_map + std::function) je ne peux pas cree de map
 	for (size_t i = 0; i < id.size(); i++) {
