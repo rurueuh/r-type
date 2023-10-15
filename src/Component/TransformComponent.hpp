baab5dd1a600a8875b37eec93322f6f41a378103
Alan Piedbois <alan@toboro.io>
2023-10-15 03:40:28 +0200

    [+] transform component

--- src/Component/TransformComponent.hpp
+++ src/Component/TransformComponent.hpp
@@ -8,11 +8,23 @@
 #pragma once
 
 #include "SFML.hpp"
+#include "ComponentBase.hpp"
 
-struct TransformComponent {
+struct TransformComponent : public Component {
     TransformComponent(sf::Vector2f position, sf::Vector2f scale, float rotation) : position(position), scale(scale), rotation(rotation) {};
     TransformComponent() : position(sf::Vector2f(0, 0)), scale(sf::Vector2f(0, 0)), rotation(0) {};
     sf::Vector2f position;
     sf::Vector2f scale;
     float rotation;
+
+    virtual std::string toString() override {
+        std::stringstream ss = std::stringstream();
+        ss << position.x << ' ' << position.y << ' ' << scale.x << ' ' << scale.y << ' ' << rotation;
+        return ss.str();
+    }
+
+    virtual void fromString(std::string str) override {
+        std::stringstream ss = std::stringstream(str);
+        ss >> position.x >> position.y >> scale.x >> scale.y >> rotation;
+    }
 };
