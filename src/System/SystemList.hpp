4d9bb9292aeba85950649ecbe8fad70c129a5458
Alan Piedbois <alan@toboro.io>
2023-10-15 03:42:59 +0200

    [+] added system list file for include

---
+++ src/System/SystemList.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-LIL-5-1-rtype-maximilien.vanstaevel
+** File description:
+** SystemList
+*/
+
+#pragma once
+
+#include "DrawableSystem.hpp"
+#include "TransformSystem.hpp"
+#include "TestSystem.hpp"
