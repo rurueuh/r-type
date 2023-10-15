babacb8c7982c86846ce67139afebab55a05f0c9
Alan Piedbois <alan@toboro.io>
2023-10-15 03:47:11 +0200

    [FIX] fixed another texture error

--- src/TextureManager/TextureManager.hpp
+++ src/TextureManager/TextureManager.hpp
@@ -9,14 +9,20 @@
 #include <map>
 #include "SFML.hpp"
 
+#ifndef SERVER
+
 class TextureManager {
     public:
         TextureManager() = default;
         ~TextureManager() = default;
 
-        static sf::Texture& getTexture(const std::string& path);
+        static sf::Texture *getTexture(const std::string& path);
 
     protected:
     private:
-      static std::map<std::string, sf::Texture> _textures;
+      static std::map<std::string, sf::Texture *> _textures;
 };
+
+inline std::map<std::string, sf::Texture *> TextureManager::_textures = std::map<std::string, sf::Texture *>();
+
+#endif
