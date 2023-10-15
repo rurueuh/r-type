53f5e9291bfd2ad04deb6246331dd85a0af9e7e7
Alan Piedbois <alan@toboro.io>
2023-10-15 03:37:15 +0200

    [+] add Transform to factory || rename sprite to drawable

--- src/Component/Component.cpp
+++ src/Component/Component.cpp
@@ -20,10 +20,18 @@ void ECS::Component::FactoryAssignPlayerInputComponent(Entity* ent, std::string 
 }
 
 void ECS::Component::FactoryAssignDrawableComponent(Entity* ent, std::string type, std::string data) {
-	auto id = Utils::getRegisteredComponent(Utils::getTypeId<SpriteComponent>());
+	auto id = Utils::getRegisteredComponent(Utils::getTypeId<DrawableComponent>());
 	if (type == id) {
-		auto sprite = ent->assign<SpriteComponent>();
+		auto sprite = ent->assign<DrawableComponent>();
 		sprite->fromString(data);
+	}
+}
+
+void ECS::Component::FactoryAssignTransformComponent(Entity* ent, std::string type, std::string data) {
+	auto id = Utils::getRegisteredComponent(Utils::getTypeId<TransformComponent>());
+	if (type == id) {
+		auto transform = ent->assign<TransformComponent>();
+		transform->fromString(data);
 	}
 }
 
@@ -33,12 +41,14 @@ void ECS::Component::FactoryAssignComponent(Entity* ent, std::string type, std::
 	std::vector<std::function<void(Entity*, std::string, std::string)>> funcs = {
 		FactoryAssignPvComponent,
 		FactoryAssignPlayerInputComponent,
-		FactoryAssignDrawableComponent
+		FactoryAssignDrawableComponent,
+		FactoryAssignTransformComponent
 	};
 	std::vector<std::string> id = {
 		Utils::getRegisteredComponent(Utils::getTypeId<PvComponent>()),
 		Utils::getRegisteredComponent(Utils::getTypeId<PlayerInputComponent>()),
-		Utils::getRegisteredComponent(Utils::getTypeId<SpriteComponent>())
+		Utils::getRegisteredComponent(Utils::getTypeId<DrawableComponent>()),
+		Utils::getRegisteredComponent(Utils::getTypeId<TransformComponent>())
 	};
 	// pour une raison inconnue (probablement le combo unordered_map + std::function) je ne peux pas cree de map
 	for (size_t i = 0; i < id.size(); i++) {
