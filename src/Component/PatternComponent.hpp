c4784a95817707692ac5bac3801ba6e3511fd5cc
odyce <ulysse.decottignies@epitech.eu>
2023-11-01 16:30:04 +0100

    [~] fixed typos and 2 bugs on the PatternSystem tick*

--- src/Component/PatternComponent.hpp
+++ src/Component/PatternComponent.hpp
@@ -3,7 +3,18 @@
 #include "ComponentBase.hpp"
 
 struct PatternComponent : public Component {
-    PatternComponent(std::string pattern) : pattern(pattern) {};
-    PatternComponent() : pattern("o") {};
-    std::string pattern;
+    PatternComponent(std::string pattern, size_t currentIndex) : pattern(pattern), currentIndex(currentIndex) {};
+    PatternComponent() : pattern("o"), currentIndex(0) {};
+
+    inline virtual std::string toString() const override {
+        return pattern;
+    }
+
+    virtual void fromString(std::string str) override {
+        pattern = str;
+        currentIndex = 0;
+    }
+
+    std::string pattern = "o";
+    size_t currentIndex = 0;
 };
